19c6f62562462d58fcb6551409b7b561856710ea
seungmin <127867905+seungmin06@users.noreply.github.com>
2024-05-16 15:26:26 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
@@ -19,6 +19,7 @@ io.on('connection', (socket) => {
   });
 
   socket.on('send_message', ({ username, room, message }) => {
+    // 여기서는 클라이언트에게 메시지를 전달하는 대신에, 방에 속한 클라이언트들에게 메시지를 전달해야 합니다.
     io.to(room).emit('receive_message', { username, message });
   });
 
